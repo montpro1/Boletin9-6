9c838eea3296cc588f8184e98e64ba577e593da4
io <io@DESKTOP-DP9DE2D>
2019-12-12 20:46:45 +0100

    ejercicios12442

---
+++ src/bol9_6/Bol9_6.java
@@ -0,0 +1,15 @@
+package bol9_6;
+
+
+
+
+
+public class Bol9_6 {
+
+   
+    public static void main(String[] args) {
+      Metodos obx=new Metodos();
+      obx.countWorkersBySalary();
+    }
+    
+}
